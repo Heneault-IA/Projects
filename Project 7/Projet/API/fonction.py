1a2c623e214d34bcaaf9eaa74f0fa48909f58ab2
Heneault Thomas <127695677+Heneault-IA@users.noreply.github.com>
2024-02-29 15:02:24 +0100

    Delete Project 7/Projet directory

--- Project 7/Projet/API/fonction.py
+++
@@ -1,8 +0,0 @@
-from sklearn.metrics import confusion_matrix
-
-def score_custom(y_val, y_pred):
-    tn, fp, fn, tp = confusion_matrix(y_val, y_pred).ravel()
-
-    score = fn * 10000 + fp * 1000 - tn * 10000
-
-    return score
